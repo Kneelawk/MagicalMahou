0861e5bf44385c282e0e1fcda75d5d0123e4a1e3
Kneelawk <kneelawk@gmail.com>
2021-06-18 13:22:20 -0700

    Add PlayerEntityRendererMixin

---
+++ src/main/java/com/kneelawk/mama/mixin/api/PlayerEntityRendererEvents.java
@@ -0,0 +1,27 @@
+package com.kneelawk.mama.mixin.api;
+
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.client.render.entity.feature.FeatureRenderer;
+import net.minecraft.client.render.entity.model.PlayerEntityModel;
+
+import java.util.function.Consumer;
+
+public class PlayerEntityRendererEvents {
+
+    public static final Event<AddFeatures> ADD_FEATURES =
+            EventFactory.createArrayBacked(AddFeatures.class, (renderer, ctx, consumer) -> {
+            }, callbacks -> (renderer, ctx, consumer) -> {
+                for (final AddFeatures callback : callbacks) {
+                    callback.addFeatures(renderer, ctx, consumer);
+                }
+            });
+
+    public interface AddFeatures {
+        void addFeatures(PlayerEntityRenderer renderer, EntityRendererFactory.Context ctx,
+                         Consumer<FeatureRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>>> consumer);
+    }
+}
