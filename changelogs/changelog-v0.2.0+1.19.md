b320d54baa658d62c673f3ef98d8102b5bb573e6
Kneelawk <kneelawk@gmail.com>
2022-07-07 20:57:06 -0700

    Improve Crystal Ball outline shape

--- changelogs/changelog-v0.2.0+1.19.md
+++ changelogs/changelog-v0.2.0+1.19.md
@@ -5,3 +5,4 @@ Magical Mahou version 0.2.0 for Minecraft 1.19
 * Updated to Minecraft 1.19.
 * Changed versioning scheme to include Minecraft version.
 * Renamed Long Fall ability to Feather Fall.
+* Improved Crystal Ball outline shape.
--- src/main/kotlin/com/kneelawk/magicalmahou/block/CrystalBallBlock.kt
+++ src/main/kotlin/com/kneelawk/magicalmahou/block/CrystalBallBlock.kt
@@ -3,6 +3,7 @@ package com.kneelawk.magicalmahou.block
 import com.kneelawk.magicalmahou.screenhandler.MMScreenHandlers
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.block.ShapeContext
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.particle.ParticleTypes
 import net.minecraft.screen.NamedScreenHandlerFactory
@@ -11,9 +12,31 @@ import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.random.Random
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.util.shape.VoxelShapes
+import net.minecraft.world.BlockView
 import net.minecraft.world.World
+import kotlin.math.sqrt
 
 class CrystalBallBlock(settings: Settings) : Block(settings) {
+    companion object {
+        private val OUTLINE = VoxelShapes.union(
+            VoxelShapes.cuboid(0.0, 0.0, 0.0, 1.0, 1.0 / 16.0, 1.0),
+            VoxelShapes.cuboid(2.0 / 16.0, 1.0 / 16.0, 2.0 / 16.0, 14.0 / 16.0, 6.0 / 16.0, 14.0 / 16.0),
+            VoxelShapes.cuboid(1.0 / 16.0, 6.0 / 16.0, 1.0 / 16.0, 15.0 / 16.0, 7.0 / 16.0, 15.0 / 16.0),
+            VoxelShapes.cuboid(
+                0.5 - sqrt(32.0) / 16.0, 7.0 / 16.0, 0.5 - sqrt(32.0) / 16.0, 0.5 + sqrt(32.0) / 16.0, 15.0 / 16.0,
+                0.5 + sqrt(32.0) / 16.0
+            )
+        )
+    }
+
+    override fun getOutlineShape(
+        state: BlockState?, world: BlockView?, pos: BlockPos?, context: ShapeContext?
+    ): VoxelShape {
+        return OUTLINE
+    }
+
     override fun onUse(
         state: BlockState, world: World, pos: BlockPos, player: PlayerEntity, hand: Hand, hit: BlockHitResult
     ): ActionResult {
